b8e09893eaa5ea675271a53713a922918df23a87
MagicMotion <MagicMotion2003@proton.me>
2021-04-12 22:17:59 +0000

    Generated source

--- consts/paths.py
+++ consts/paths.py
@@ -5,4 +5,6 @@ DATA_PATH = os.path.join(PROJECT_PATH, 'data')
 
 
 class Paths:
-    dataset_path = os.path.join(DATA
+    dataset_path = os.path.join(DATA_PATH, 'tweets.csv')
+    train_dataset_path = os.path.join(DATA_PATH, 'train_tweets.csv')
+    test_dataset_path
