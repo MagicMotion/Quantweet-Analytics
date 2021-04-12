46d76f02977fb92c70c3c5153fe2bcdb457dd6a2
MagicMotion <MagicMotion2003@proton.me>
2021-04-12 21:55:58 +0000

    Several updates

--- consts/paths.py
+++ consts/paths.py
@@ -1,3 +1,8 @@
 import os
 
-PROJECT_PATH = os.path.abspath(os.path.join(__file__, *(os.
+PROJECT_PATH = os.path.abspath(os.path.join(__file__, *(os.path.pardir,) * 2))
+DATA_PATH = os.path.join(PROJECT_PATH, 'data')
+
+
+class Paths:
+    dataset_path = os.path.join(DATA
