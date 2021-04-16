ba67c8e1443d8a929692ac2572e2e9afee0ec9ee
MagicMotion <MagicMotion2003@proton.me>
2021-04-16 07:22:05 +0000

    updated dependencies.

--- consts/paths.py
+++ consts/paths.py
@@ -7,4 +7,5 @@ DATA_PATH = os.path.join(PROJECT_PATH, 'data')
 class Paths:
     dataset_path = os.path.join(DATA_PATH, 'tweets.csv')
     train_dataset_path = os.path.join(DATA_PATH, 'train_tweets.csv')
-    test_dataset_path
+    test_dataset_path = os.path.join(DATA_PATH, 'test_tweets.csv')
+    val_dataset_path = os.path.join(DATA_PATH, 'val_tweets.csv')
---
+++ dataset/dataset.py
@@ -0,0 +1,14 @@
+
+from numpy import dtype
+import torch
+
+
+class Dataset:
+
+    def read_data(self, dataset):
+        
+        print('Reading data...')
+        labels = dataset['target'].unique().tolist()
+        sentences = dataset['text'].tolist()
+
+        return torch.as_tensor(labels, dtype=torch.float32), sentences
