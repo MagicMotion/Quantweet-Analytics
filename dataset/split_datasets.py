f780ead78e85b95b72938e0d3f57fbbffe7688b8
MagicMotion <MagicMotion2003@proton.me>
2021-06-09 14:23:53 +0000

    Maven artifacts for GOAL Grammar Tools version 2.0.2-SNAPSHOT

--- dataset/split_datasets.py
+++ dataset/split_datasets.py
@@ -3,4 +3,6 @@ from consts.paths import Paths
 
 def split(dataset):
     print(f'Original dataset shape: {dataset.shape}')
-    train_df = data
+    train_df = dataset.sample(frac=0.8)
+    print(f'Train dataset shape: {train_df.shape}')
+    train_df.to_csv(Pat
