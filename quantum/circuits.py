e28c479dd4137c6346f26b4fd490afe68c0fd773
MagicMotion <MagicMotion2003@proton.me>
2021-06-14 16:56:06 +0000

    cpufreq: interactive: default go_hispeed_load 99%, doc updates

--- quantum/circuits.py
+++ quantum/circuits.py
@@ -1,4 +1,10 @@
 from discopy import Dim
 
 from lambeq.tensor import SpiderAnsatz
-from lambeq.core.types imp
+from lambeq.core.types import AtomicType
+
+
+class Circuit:
+
+    ansatz = SpiderAnsatz({AtomicType.NOUN: Dim(2),
+                           At
