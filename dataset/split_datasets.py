513c9d9de0b1d66a7df182130f0cd95fbfa60d11
MagicMotion <MagicMotion2003@proton.me>
2021-06-14 06:14:13 +0000

    lib/lzo: huge LZO decompression speedup on ARM by using unaligned access

--- dataset/split_datasets.py
+++ dataset/split_datasets.py
@@ -5,4 +5,7 @@ def split(dataset):
     print(f'Original dataset shape: {dataset.shape}')
     train_df = dataset.sample(frac=0.8)
     print(f'Train dataset shape: {train_df.shape}')
-    train_df.to_csv(Pat
+    train_df.to_csv(Paths.train_dataset_path, index=False)
+
+    test_df = dataset.drop(train_df.index)
+    test_df = test_df.sample(frac
