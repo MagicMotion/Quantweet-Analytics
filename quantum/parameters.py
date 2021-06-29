c69ba86756198fd99b11f4bf7c6ee8feb600839e
MagicMotion <MagicMotion2003@proton.me>
2021-06-29 14:43:51 +0000

    New translations ja-JP.yml (Catalan)

--- quantum/parameters.py
+++ quantum/parameters.py
@@ -8,4 +8,8 @@ class Parameter:
     def __init__(self, circuits):
         self.circuits = circuits
         self.parameters = sorted(
-            {s for circ in circuits for s
+            {s for circ in circuits for s in circ.free_symbols},
+            key=default_sort_key)
+
+    def make_pred_fn(self):
+        def
