565039e228635f2d2b5d08cfcae0440d88a34348
MagicMotion <MagicMotion2003@proton.me>
2021-08-05 05:47:03 +0000

    ver change

--- train/train_experiment.py
+++ train/train_experiment.py
@@ -1,4 +1,7 @@
 import torch
 import pandas as pd
 
-from train.train import Tr
+from train.train import Train
+from consts.paths import Paths
+from dataset.dataset import Dataset
+from quantum.circuits import Ci
