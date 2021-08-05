223dfe7e811de5d922543c7e0d4a8bff20b8cdfe
MagicMotion <MagicMotion2003@proton.me>
2021-08-05 15:08:13 +0000

    Update _svg-icons.scss

--- train/train_experiment.py
+++ train/train_experiment.py
@@ -4,4 +4,10 @@ import pandas as pd
 from train.train import Train
 from consts.paths import Paths
 from dataset.dataset import Dataset
-from quantum.circuits import Ci
+from quantum.circuits import Circuit
+from quantum.diagrams import Diagram
+from quantum.parameters import Parameter
+from dataset.preprocess import Preprocess
+
+
+datas
